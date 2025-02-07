8a71031bc8efc2feca74b04daf7bb390e8ac014a
valmo-dev <valmo.dev@gmail.com>
2025-02-07 12:44:26 +0100

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-bold underline mb-8">Hello World</h1>
+
+    <HelloWorld />
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="card lg:card-side bg-base-100 shadow-sm">
+    <figure>
+      <img
+        src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+        alt="Album"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">Hello world !</h2>
+      <p>Click the button to listen on Spotiwhy app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Listen</button>
+      </div>
+    </div>
+  </div>
+</template>
