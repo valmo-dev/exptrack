3a24e7ec96cbb369818b980f31d8704399f0c7c2
valmo-dev <valmo.dev@gmail.com>
2025-02-10 12:18:09 +0100

    feat(components): add owner selection to BudgetForm

--- src/components/BudgetForm.vue
+++ src/components/BudgetForm.vue
@@ -8,6 +8,7 @@ function useBudgetForm() {
   const budgetStore = useBudgetStore();
 
   const type = ref("Revenu");
+  const owner = ref("Marine");
   const category = ref("");
   const amount = ref(0);
 
@@ -15,61 +16,88 @@ function useBudgetForm() {
     if (category.value.trim() && amount.value > 0) {
       const newItem = {
         type: type.value,
+        owner: owner.value,
         category: category.value,
         amount: amount.value,
       };
+
       budgetStore.addItem(newItem);
+
+      // * Reset les champs du formulaire
       category.value = "";
       amount.value = 0;
       type.value = "Revenu";
+      owner.value = "Marine";
     }
   }
 
   return {
     type,
+    owner,
     category,
     amount,
     submitForm,
   };
 }
 
-const { type, category, amount, submitForm } = useBudgetForm();
+const { type, owner, category, amount, submitForm } = useBudgetForm();
 </script>
 
 <template>
   <div class="p-6">
     <h2 class="text-2xl font-bold mb-4">Ajouter une entrée</h2>
 
-    <form @submit.prevent="submitForm" class="space-y-4 grid grid-cols-1 gap-4">
-      <div>
-        <label for="type" class="fieldset-label">Type d'entrée</label>
-        <select id="type" v-model="type" class="select w-full">
-          <option value="Revenu">Revenus</option>
-          <option value="Charge">Charges</option>
-        </select>
+    <form @submit.prevent="submitForm" class="space-y-6">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div class="form-control">
+          <label for="type" class="fieldset-label my-2">Type d'entrée</label>
+          <select
+            id="type"
+            v-model="type"
+            class="select select-bordered focus:select-primary w-full"
+          >
+            <option value="Revenu">Revenus</option>
+            <option value="Charge">Charges</option>
+          </select>
+        </div>
+        <div class="form-control">
+          <label for="type" class="fieldset-label my-2">Assigné à</label>
+          <select
+            v-model="owner"
+            class="select select-bordered focus:select-primary w-full"
+          >
+            <option>Marine</option>
+            <option>Valentin</option>
+            <option>Commun</option>
+          </select>
+        </div>
       </div>
-      <div>
-        <label for="category" class="fieldset-label">Catégorie</label>
-        <input
-          id="category"
-          v-model="category"
-          type="text"
-          placeholder="Exemple : Salaire, loyer, nourriture, etc."
-          class="input input-bordered w-full"
-          required
-        />
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div class="form-control">
+          <label for="category" class="fieldset-label my-2">Catégorie</label>
+          <input
+            id="category"
+            v-model="category"
+            type="text"
+            placeholder="Ex: Salaire, loyer, nourriture, etc."
+            class="input input-bordered focus:input-primary w-full"
+            required
+          />
+        </div>
+        <div class="form-control">
+          <label for="amount" class="fieldset-label my-2">Montant (€)</label>
+          <input
+            id="amount"
+            v-model.number="amount"
+            type="number"
+            class="input input-bordered focus:input-primary w-full"
+            required
+          />
+        </div>
       </div>
-      <div>
-        <label for="amount" class="fieldset-label">Montant (€)</label>
-        <input
-          id="amount"
-          v-model.number="amount"
-          type="number"
-          class="input input-bordered w-full"
-          required
-        />
-      </div>
-      <button type="submit" class="btn btn-primary">Ajouter</button>
+
+      <button type="submit" class="btn btn-primary w-full">Ajouter</button>
     </form>
   </div>
 </template>
